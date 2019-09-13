a4d64da2e5adcc91283bb27e959fdc1106af6c84
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-09-13 11:46:34 +0530

    restructure the module

--- src/app/modules/visualizer/visualizer.module.ts
+++ src/app/modules/visualizer/visualizer.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { VisualizerRoutingModule } from './visualizer-routing.module';
 import { MainComponent } from './components/main/main.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { GraphComponent } from './components/graph/graph.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, ChartComponent, GraphComponent],
   imports: [
     CommonModule,
     VisualizerRoutingModule
