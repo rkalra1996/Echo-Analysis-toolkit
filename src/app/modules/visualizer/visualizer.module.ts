2d4a127f0b760168f9dfe3beae9565de83cf76fe
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-09-13 11:30:42 +0530

    added basic module

---
+++ src/app/modules/visualizer/visualizer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VisualizerRoutingModule } from './visualizer-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VisualizerRoutingModule
+  ]
+})
+export class VisualizerModule { }
