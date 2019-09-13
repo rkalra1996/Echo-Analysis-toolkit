6fb4cb91247687280934c59f6dcbcfb880d0b125
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-09-13 11:37:01 +0530

    changed default styling to scss and created d3 module

--- src/app/modules/visualizer/visualizer.module.ts
+++ src/app/modules/visualizer/visualizer.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VisualizerRoutingModule } from './visualizer-routing.module';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent],
   imports: [
     CommonModule,
     VisualizerRoutingModule
